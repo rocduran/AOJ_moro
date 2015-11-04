66135fbbc5cd0012e23a96a2ddfb4f86789fab4f
Roc Duran <rocduran@gmail.com>
2015-11-04 23:31:03 +0100

    Començat JavaDoc de Servei
    @sesiomterim He començat el JavaDoc de la Entity Servei. Dona més feina del que em pensava pero crec que es un bon inici. No recordo com cony es mirava el que sortia del JavaDoc aixi que demà ens mirem com ha quedat XD

--- ejbModule/ad/uda/moro/ejb/entity/Servei.java
+++ ejbModule/ad/uda/moro/ejb/entity/Servei.java
@@ -3,22 +3,29 @@ package ad.uda.moro.ejb.entity;
 import java.io.Serializable;
 import javax.persistence.*;
 
+/**
+ * Class <code><b>Servei</b></code>. Entity <code><b>Servei</b></code>. Taula persistency <code><b>servei</b></code>
+ * @author rduran & mmiret
+ *
+ */
+
 @Entity(name="Servei")
 @Table(name="servei")
 
 public class Servei implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	/**
-	 * Class <code><b>Servei</b></code> 
-	 * @author rduran & mmiret
-	 *
-	 */
 	
 	@Id @Column(name ="id", length=11) private int id;
 	@Column(name = "idTipus", length=11) private int idTipus;
 	@Column(name="descripcio", length=255) private String descripcio;
 	
+	/**
+	 * Constructor method amb parametres:
+	 * @param id Identificador del servei (PK)
+	 * @param idTipus Identificador del tipus de servei.
+	 * @param descripcio Descriu que es el servei en un String.
+	 */
 	public Servei(int id, int idTipus, String descripcio) {
 		super();
 		this.id = id;
@@ -26,32 +33,61 @@ public class Servei implements Serializable {
 		this.descripcio = descripcio;
 	}
 	
+	/**
+	 * Constructor method sense parametres.
+	 */
 	public Servei(){}
 
+	/**
+	 * Getter de id unic del servei:
+	 * @return El id unic del servei
+	 */
 	public int getId() {
 		return id;
 	}
-
+	
+	/**
+	 * Setter de id unic del servei:
+	 * @param identificador unic del servei
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Getter de idTipus de servei:
+	 * @return El id unic del servei
+	 */
 	public int getIdTipus() {
 		return idTipus;
 	}
 
+	/**
+	 * Setter de idTipus de servei:
+	 * @param identificador del tipus de servei
+	 */
 	public void setIdTipus(int idTipus) {
 		this.idTipus = idTipus;
 	}
 
+	/**
+	 * Getter de la descripcio de servei:
+	 * @return La descripcio del servei, per exemple: "<code><b>Guia Paku</b></code>"
+	 */
 	public String getDescripcio() {
 		return descripcio;
 	}
-
+	/**
+	 * Setter de idTipus de servei:
+	 * @param identificador del tipus de servei
+	 */
 	public void setDescripcio(String descripcio) {
 		this.descripcio = descripcio;
 	}
 
+	/**
+	 * Mètode toString de Servei per possibles debuggings.
+	 */
 	@Override
 	public String toString() {
 		return "Servei [id=" + id + ", idTipus=" + idTipus + ", descripcio=" + descripcio + "]";
